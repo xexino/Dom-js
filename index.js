1e119708ccc8b1f3963efbd61969cef4563527aa
ayman <aymanachraf890@gmail.com>
2021-08-25 13:00:47 +0100

    decrese update

--- index.js
+++ index.js
@@ -1,60 +1,121 @@
+// creer une liste de commandes
+
 var COMMANDES = [
-
     {
-        title: 'Laptop ',
+
+        id: 1, title: 'Laptop Case',
         prix: 20.34,
-        thumnail: "img/illu-tablette.jpg",
-        quantity : 1  
+        thumnail: "laptop-case.jpg",
+        quantity: 4
     },
-
     {
-        title: 'Laptop ',
-        prix: 20.34,
-        thumnail: "img/illu-tablette.jpg",
-        quantity : 1  
-   },
 
-    {
-        title: 'Laptop ',
+        id: 2, title: 'Phone Case',
         prix: 20.34,
-        thumnail: "img/illu-tablette.jpg",
-        quantity : 1  
-  },
-
-    {
-        title: 'Laptop ',
+        thumnail: "laptop-case.jpg",
+        quantity: 1
+    }, {
+        id: 3,
+        title: 'Clavier Case',
         prix: 20.34,
-        thumnail: "img/illu-tablette.jpg",
-        quantity : 1  
- },
-
+        thumnail: "laptop-case.jpg",
+        quantity: 1
+    }, {
+        id: 4,
+        title: 'Pencil Case',
+        prix: 20.34,
+        thumnail: "laptop-case.jpg",
+        quantity: 1
+    },
 ]
 
-// affichage
-
-var list_command = document.getElementById("list-commands");
-
-//parcourir la liste des commands 
-for (var command of COMMANDES) {
-    //
-    var shopping_item = `
-    <section>
-    <div class="product">
-        <div class="descr">
-            <img src="${command.thumnail}" alt="macBook">
-            <p> ${command.title} <br> ${command.prix}$</p>
-        </div>
-        <div class="butt">
-            <button>-</button>
-            <input type="number" value="${command.quantity}">
-            <button>+</button>
-            <button><i class="fas fa-trash-alt"></i></button>
-        </div>
-    </div>
-    </section>`
+//Afficher la liste des commandes 
+AfficherShoppingCart()
 
 
-    list_command.innerHTML += shopping_item
+
+
+
+
+
+
+////------- Functions --------------
+function onDecreseQuantity(id){
+    //chercher command par lid 
+    for (const command of COMMANDES) {
+        if(command?.id==id && command.quantity >1){
+            command.quantity--;
+        }
+    }
+    AfficherShoppingCart()
+    console.table(COMMANDES)
+}
+
+
+
+
+function onAddQuantity(id){
+    //chercher command par lid 
+    for (const command of COMMANDES) {
+        if(command?.id==id){
+            command.quantity++;
+        }
+    }
+    AfficherShoppingCart()
+    console.table(COMMANDES)
+}
+
+
+function onDeleteItem(id) {
+    var choice = confirm("Etes vous sure ?");
+    if (choice) {
+        // alert(" suppression de la commd Id :"+id)
+        //chercher lelm avec lid selectionner et le supprimer avec delete 
+        for (var i = 0; i < COMMANDES.length; i++) {
+            if (COMMANDES[i] != undefined
+                && COMMANDES[i].id == id)
+                delete COMMANDES[i]
+        }
+        AfficherShoppingCart();
+    }
+
 
 }
-console.log(list_command) 
+
+
+function AfficherShoppingCart(){
+     // je cible ul 
+        var list_command = document.getElementById("list-commands");
+        list_command.innerHTML = ""
+        //parcourir la liste des commands 
+        for (var command of COMMANDES) {
+            //
+            if (command != undefined) {
+                var shopping_item = `
+            <li class="shopping-cart-item">
+            <div>
+                <img src="${command.thumnail}" alt="${command.title}" />
+                <p class="item-info">
+                    <span>${command.title}</span>
+                    <span>${command.prix}$</span>
+                </p>
+            </div>
+            <div>
+                <button onclick="onDecreseQuantity(${command.id})">-</button>
+                <input type="number" value="${command.quantity}" />
+                <button onclick="onAddQuantity(${command.id})">+</button>
+                <button onclick="onDeleteItem(${command.id})">
+                    <i class="fa fa-trash-alt"></i>
+                </button>
+            </div>
+            </li> 
+            `
+                //ajouter a la liste ul 
+                list_command.innerHTML += shopping_item
+
+
+            }
+
+        }
+
+    }
